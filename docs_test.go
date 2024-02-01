bb66cc322230480a7419dfca56b68de0e4cdbdf5
DanRoscigno <dan@roscigno.com>
2024-01-31 21:43:38 -0500

    just need to fix dsn
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs_test.go
+++ docs_test.go
@@ -2,14 +2,38 @@ package docs_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	//. "github.com/onsi/gomega"
-	//"context"
-	//"database/sql"
+	. "github.com/onsi/gomega"
+	"database/sql"
 	//"fmt"
 	//"time"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
-var _ = Describe("Docs", func() {
 
+var _ = Describe("Docs", func() {
+	When("instance is running in staging", Ordered, func() {
+		var db *sql.DB
+		cfg := mysql.Config{
+			User:   "root",
+			Passwd: "",
+			Net:    "tcp",
+			Addr:   "127.0.0.1:9030",
+			DBName: "DocsQA",
+		}
+
+		BeforeEach(func() {
+			db, _ = sql.Open("mysql", cfg.FormatDSN())
+		})
+
+		AfterAll(func() {
+			_, err := db.Exec(`DROP DATABASE IF EXISTS DocsQA`)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should be able to run SQL commands", func() {
+			By("creating a database")
+			_, err := db.Exec(`CREATE DATABASE IF NOT EXISTS DocsQA`)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
 })
