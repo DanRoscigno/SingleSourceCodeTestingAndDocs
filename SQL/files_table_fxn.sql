6d6fcf905162adf80bd094cb9dd133a5c557bdd3
DanRoscigno <dan@roscigno.com>
2024-02-01 14:15:51 -0500

    trying plugin
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- SQL/files_table_fxn.sql
+++ SQL/files_table_fxn.sql
@@ -4,6 +4,8 @@ AS SELECT * FROM FILES (
 	"path" = "s3://starrocks-examples/user_behavior_ten_million_rows.parquet",
 	"format" = "parquet",
 	"aws.s3.region" = "us-east-1",
+	-- highlight-start
 	"aws.s3.access_key" = "AAAAAAAAAAAAAAAAAAAA",
 	"aws.s3.secret_key" = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+	-- highlight-end
 );
