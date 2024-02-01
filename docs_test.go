8852b522efff3d698a2f43f479e1c34ea3f67e64
DanRoscigno <dan@roscigno.com>
2024-01-31 22:52:24 -0500

    popuate table
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docs_test.go
+++ docs_test.go
@@ -25,16 +25,35 @@ var _ = Describe("Docs", func() {
 
 		BeforeEach(func() {
 			db, _ = sql.Open("mysql", cfg.FormatDSN())
+			db.SetMaxOpenConns(1)
 		})
 
 		AfterAll(func() {
-			_, err := db.Exec(`DROP DATABASE IF EXISTS DocsQA`)
+			_, err := db.Exec(`DROP DATABASE IF EXISTS DocsQA2`)
 			Expect(err).ToNot(HaveOccurred())
 		})
 
 		It("should be able to run SQL commands", func() {
 			By("creating a database")
 			_, err := db.Exec(`CREATE DATABASE IF NOT EXISTS DocsQA`)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should be able to run SQL commands", func() {
+			By("choosing a database")
+			_, err := db.Exec(`USE DocsQA`)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should be able to run SQL commands", func() {
+			By("setting the number of replicas")
+			_, err := db.Exec(`ADMIN SET FRONTEND CONFIG ('default_replication_num' = "1");`)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should be able to run SQL commands", func() {
+			By("creating and populating a table")
+			_, err := db.Exec(`CREATE TABLE DocsQA.user_behavior_inferred AS SELECT * FROM FILES ( "path" = "s3://starrocks-examples/user_behavior_ten_million_rows.parquet", "format" = "parquet", "aws.s3.region" = "us-east-1", "aws.s3.access_key" = "AKIAZWYRUJIJ3S43RPOA", "aws.s3.secret_key" = "JL+uV5hb6zm5p8m4yWy9iTD5qIn8YGbtv0qYr9qJ");`)
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
