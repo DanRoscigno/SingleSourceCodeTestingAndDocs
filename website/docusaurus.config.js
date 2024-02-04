58cb3e74b1d8ef853185c63fd30cc211116b5954
DanRoscigno <dan@roscigno.com>
2024-02-04 17:31:00 -0500

    docusaurus to gh pages
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://SingleSourceCodeTestingAndDocs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DanRoscigno', // Usually your GitHub org/user name.
+  projectName: 'SingleSourceCodeTestingAndDocs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'ignore',
