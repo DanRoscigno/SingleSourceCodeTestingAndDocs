282fe9bb366ec70bc55708608867a33f5c050b07
DanRoscigno <dan@roscigno.com>
2024-02-04 17:55:34 -0500

    conf
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://danroscigno.github.io/SingleSourceCodeTestingAndDocs',
+  url: 'https://danroscigno.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/SingleSourceCodeTestingAndDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
