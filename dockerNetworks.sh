f6c8a638a6592c609f6e8634f318aefe6021c896
DanRoscigno <dan@roscigno.com>
2024-02-09 15:16:37 -0500

    set network env var
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- dockerNetworks.sh
+++ dockerNetworks.sh
@@ -1,7 +1,7 @@
 NET=`docker network ls --format json| \
   jq 'select(.Driver | contains("bridge")) | {Name: .Name}' | \
   jq 'select(.Name != "bridge")' | jq .Name | tr -d \"`
-  echo "mynet=${NET}" > .env
-  echo "mynet=${NET}"
+  echo "network=${NET}" > .env
+  echo "network=${NET}"
 
 
