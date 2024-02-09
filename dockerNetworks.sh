490e5b97d1bfb592b10745c2723fe6502ef8bdb5
DanRoscigno <dan@roscigno.com>
2024-02-09 15:12:19 -0500

    show network
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- dockerNetworks.sh
+++ dockerNetworks.sh
@@ -2,5 +2,6 @@ NET=`docker network ls --format json| \
   jq 'select(.Driver | contains("bridge")) | {Name: .Name}' | \
   jq 'select(.Name != "bridge")' | jq .Name | tr -d \"`
   echo "mynet=${NET}" > .env
+  echo "mynet=${NET}"
 
 
